8ad4d82d220fbd05febcab98eb64ec933b7a61bd
Martin Rimbro Åstrand <martinrimbroastrand@MacBookPro.localdomain>
2025-09-30 16:07:15 +0200

    add an image carousel

---
+++ frontend/src/containers/carousel/Carousel.tsx
@@ -0,0 +1,9 @@
+import { Poster } from '../../components'
+
+export default function Carousel() {
+    return (
+        <>
+            <Poster src="https://a.ltrbxd.com/resized/film-poster/9/5/7/0/5/0/957050-superman-2025-0-230-0-345-crop.jpg?v=54e41a55ff" alt="superman" />
+        </>
+    )
+}
