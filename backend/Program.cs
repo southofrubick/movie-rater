afc5557d3a7d72b4181e840bf398056e85ecdc8e
Martin Rimbro Åstrand <martinrimbroastrand@MacBookPro.localdomain>
2025-09-30 18:33:25 +0200

    add cors and get popular movies

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,7 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +30,44 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+Movie oneBattleAfterAnother = new Movie("tt30144839", "https://a.ltrbxd.com/resized/film-poster/9/5/1/2/7/7/951277-one-battle-after-another-0-230-0-345-crop.jpg?v=d27c4cc662", "One Battle After Another", 5.0, "2025");
+Movie superman = new Movie("tt5950044", "https://a.ltrbxd.com/resized/film-poster/9/5/7/0/5/0/957050-superman-2025-0-230-0-345-crop.jpg?v=54e41a55ff", "Superman", 4.0, "2025");
+Movie theLongWalk = new Movie("tt6800268", "https://a.ltrbxd.com/resized/film-poster/5/3/1/3/1/6/531316-the-long-walk-2025-0-230-0-345-crop.jpg?v=9cf0c245aa", "The Long Walk", 3.5, "2025");
+Movie weapons = new Movie("tt26581740", "https://a.ltrbxd.com/resized/film-poster/9/7/2/1/0/9/972109-weapons-2025-0-230-0-345-crop.jpg?v=8100270337", "Weapons", 4.5, "2025");
+Movie fantasticFour = new Movie("tt10676052", "https://a.ltrbxd.com/resized/film-poster/5/4/3/9/6/7/543967-the-fantastic-four-first-steps-0-230-0-345-crop.jpg?v=ab7c919b04", "Fantastic 4", 3.5, "2025");
+
+var popularMovies = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    oneBattleAfterAnother, superman, theLongWalk, weapons, fantasticFour
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/movies/popular", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return popularMovies;
 })
-.WithName("GetWeatherForecast")
+.WithName("GetPopularMovies")
 .WithOpenApi();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+class Movie
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string ImdbID { get; set; }
+    public string Src { get; set; }
+    public string Title { get; set; }
+    public double Rating { get; set; }
+    public string Year { get; set; }
+
+    public Movie(string imdbID, string src, string title, double rating, string year)
+    {
+        ImdbID = imdbID;
+        Src = src;
+        Title = title;
+        Rating = rating;
+        Year = year;
+    }
+}
+
+record PopularMovies(string src, string title, double rating, string year)
+{
 }
