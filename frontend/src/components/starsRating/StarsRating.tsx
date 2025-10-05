fade8b7515cfd7b2bdc745ec48ab6057d9405d2d
Martin Rimbro Åstrand <martinrimbroastrand@MacBookPro.localdomain>
2025-10-05 17:45:42 +0200

    add stars component

---
+++ frontend/src/components/starsRating/StarsRating.tsx
@@ -0,0 +1,7 @@
+import './StarsRating.css'
+
+export default function StarsRating(props: { averageRating: number }) {
+    const { averageRating } = props
+
+    return <input readOnly type="range" min="0.5" step="0.5" value={averageRating} max="5" id="stars" />
+}
