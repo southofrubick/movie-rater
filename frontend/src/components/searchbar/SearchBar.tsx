de3647e5ac51dbd83e39ed6c02977d896e684aaa
Martin Rimbro Åstrand <martinrimbroastrand@MacBookPro.localdomain>
2025-10-05 17:45:18 +0200

    add searchbar component

--- frontend/src/components/searchbar/SearchBar.tsx
+++ frontend/src/components/searchbar/SearchBar.tsx
@@ -1,5 +1,71 @@
+import { useEffect, useState } from 'react'
 import './SearchBar.css'
+import { searchForMovie } from 'api'
+import { MovieCard, MovieModal } from '../'
+import { useModal } from 'hooks'
+import { postRating } from 'api'
+import type { Movie } from 'types'
+
+const DEBOUNCE_TIMER = 300
 
 export default function SearchBar() {
-    return (<input type="text" placeholder="Search.." />)
+    const [searchPhrase, setSearchPhrase] = useState('')
+    const [movie, setMovie] = useState(null as null | Movie)
+    const [error, setError] = useState(null as null | Error)
+    const [showResult, setShowResult] = useState(false)
+    const { currentModal, openModal, closeModal } = useModal()
+
+    const handleChange = (value: string) => {
+        setSearchPhrase(value)
+    }
+
+    const handleLeaveReview = () => {
+        openModal('movie-modal')
+        setShowResult(false)
+    }
+
+    const handleSubmit = (submitProps: { id?: string, newRating: number, newComment: string }) => {
+        const { newRating, newComment } = submitProps
+
+        postRating(movie!.imdbID, newRating, newComment).then(() => {
+            closeModal()
+        })
+    }
+
+    useEffect(() => {
+        if (searchPhrase.length > 0) {
+            const delayDebounce = setTimeout(() => {
+                searchForMovie(searchPhrase).then((data) => {
+                    const { error, result } = data
+
+                    if (error) {
+                        setError(error)
+                        setMovie(null)
+                    } else if (result) {
+                        setMovie(result)
+                    }
+                })
+
+                setShowResult(true)
+            }, DEBOUNCE_TIMER)
+
+            return () => clearTimeout(delayDebounce)
+        } else {
+            setMovie(null)
+            setShowResult(false)
+        }
+    }, [searchPhrase])
+
+
+    return (
+        <>
+            <input type="text" placeholder="Search.." value={searchPhrase} onChange={(event) => handleChange(event.target.value)} onFocus={() => { if (movie) { setShowResult(true) } }} />
+            {showResult && (
+                <MovieCard movie={movie} error={error} leaveReview={handleLeaveReview} />
+            )}
+            {movie && currentModal === 'movie-modal' && (
+                <MovieModal movie={movie} onSubmit={handleSubmit} closeModal={closeModal} />
+            )}
+        </>
+    )
 }
