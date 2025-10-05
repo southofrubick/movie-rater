2a8e2db518ceae3fab8b8d9657e9347786ba2d48
Martin Rimbro Åstrand <martinrimbroastrand@MacBookPro.localdomain>
2025-10-05 19:46:03 +0200

    update stars rating styling, add rating as tooltip

--- frontend/src/components/starsRating/StarsRating.tsx
+++ frontend/src/components/starsRating/StarsRating.tsx
@@ -3,5 +3,5 @@ import './StarsRating.css'
 export default function StarsRating(props: { averageRating: number }) {
     const { averageRating } = props
 
-    return <input readOnly type="range" min="0.5" step="0.5" value={averageRating} max="5" id="stars" />
+    return <input readOnly type="range" min="0.5" step="0.5" value={averageRating} max="5" id="stars" title={averageRating + "/5"} />
 }
